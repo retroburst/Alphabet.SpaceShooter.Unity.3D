8e5eebb4e960509b609c5498fb391fe0286aeb62
Andrew D <retroburst@gmail.com>
2015-06-20 16:02:52 +0900

    Before refactor

---
+++ Assets/Scripts/MoverTextEnemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverTextEnemy : MonoBehaviour
+{
+	public float speed;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = transform.up * speed;
+	}
+}
